6b785b43dcfb76cf0fcfefbb928442db4549cc31
Samuel Souza <silva.samueldev@gmail.com>
2025-11-22 11:15:09 -0300

    :wrench: feat: injetando dependencia na controller do bibliotecario

--- src/main/java/com/grupo04/Biblioteca/controllers/BibliotecarioController.java
+++ src/main/java/com/grupo04/Biblioteca/controllers/BibliotecarioController.java
@@ -1,8 +1,13 @@
 package com.grupo04.Biblioteca.controllers;
 
+import com.grupo04.Biblioteca.repository.BibliotecarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/bibliotecario")
-public class BibliotecarioController { }
+public class BibliotecarioController {
+    @Autowired
+    public BibliotecarioRepository repository;
+}

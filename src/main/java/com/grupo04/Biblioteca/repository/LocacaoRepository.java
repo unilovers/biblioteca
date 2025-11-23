1c12f3ca52ec30412ba0cb0ef2e87e56bf0851ba
KalebeFurlan <kalebefurlan@gmail.com>
2025-11-23 18:56:24 -0300

    Revert ":sparkles: feat: Adicionar Entidade LocacaoRepository"
    This reverts commit 197956afaff5a2c3f1f889327d9a1a2a797f0cb3.

--- src/main/java/com/grupo04/Biblioteca/repository/LocacaoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.grupo04.Biblioteca.repository;
-
-import com.grupo04.Biblioteca.models.LocacaoModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface LocacaoRepository extends JpaRepository<LocacaoModel, Long> {
-}

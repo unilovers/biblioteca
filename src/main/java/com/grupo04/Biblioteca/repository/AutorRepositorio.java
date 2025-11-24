001db5108b63772f245bab93c2e8baaa2b73aa45
TecGustaF <168040734+TecGustaF@users.noreply.github.com>
2025-11-24 14:18:59 -0300

    Refactor AutorRepositorio to use JpaRepository

--- src/main/java/com/grupo04/Biblioteca/repository/AutorRepositorio.java
+++ src/main/java/com/grupo04/Biblioteca/repository/AutorRepositorio.java
@@ -1,5 +1,8 @@
 package com.grupo04.Biblioteca.repository;
 
-public class AutorRepositorio {
-    
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.grupo04.Biblioteca.models.Autor;
+
+public interface AutorRepository extends JpaRepository<Autor, Integer> {
+
 }
